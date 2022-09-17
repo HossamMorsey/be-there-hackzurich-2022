1ddcdc4bf24eaf3b37f1c41a1fb5073d9c1bd6c1
Hossam Morsey <hosammorsey@gmail.com>
2022-09-17 11:43:04 +0300

    [Edit] new image added for logo

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,1 @@
-export { default as mainImg } from "../assets/images/desktop2.jpg";
-export { default as mainIcon } from "../assets/icons/artBoard.png";
-export { default as chooseUs } from "../assets/images/chooseUs.jpg";
-export { default as desktop2 } from "../assets/images/desktop2.jpg";
-// export { default as mainHome } from "../assets/images/resizeHome.jpeg";
-export { default as homeImg } from "../assets/images/resizedHome.jpeg";
+export { default as demo_logo } from "../assets/images/demo_logo.png";
